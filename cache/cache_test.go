0c58cabdda5c5bb6fc9aa799c7905ae4f2ea06ef
Ben Aaron <ben@theaarons.com>
2023-02-20 11:39:14 -0700

    get cache.New tests to passing state

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -2,9 +2,20 @@ package cache
 
 import (
 	"fmt"
+	"os"
+	"path"
 	"testing"
+
+	"github.com/google/uuid"
 )
 
 func TestDiscovery(t *testing.T) {
 	fmt.Println("tests discovered")
+}
+
+func TestNew(t *testing.T) {
+	id := uuid.New()
+    testPath := path.Join(os.TempDir(), id.String())
+    defer os.RemoveAll(testPath)
+    New(10, testPath)
 }
